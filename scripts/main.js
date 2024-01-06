628a281dc290d88962c542a96067d7b0be326b66
Mrezadwiprasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2024-01-06 21:50:20 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+Planets.erekir.ruleSetter=r=>{
+  r.infiniteResources=true;
+  r.waveTeam = Team.malis;
+  r.placeRangeCheck = false;
+  r.showSpawns = true;
+  r.fog = true;
+  r.staticFog = true;
+  r.lighting = false;
+  r.coreDestroyClear = true;
+  r.onlyDepositCore = true;
+};
+Planets.serpulo.ruleSetter=r=>{
+  r.infiniteResources=true;
+  r.waveTeam=Team.crux;
+  r.placeRangeCheck=false;
+  r.showSpawns=false;
+};
+Planets.tantros.ruleSetter=r=>{
+  r.infiniteResources=true;
+};
